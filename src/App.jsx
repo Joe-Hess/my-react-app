8045c8eec82211f8d39698f2ed76f96f04a8eb50
Joe-Hess <hess.j@wustl.edu>
2025-04-02 01:19:18 -0500

    Step #9 - Simplified Square()

--- src/App.jsx
+++ src/App.jsx
@@ -23,10 +23,6 @@ export default function Board() {
   );
 }
 
-function Square() {
-  const [value, setValue] = useState(null);
-  function handleClick() {
-    setValue('X');
-  }
-  return (<button className="square" onClick={handleClick}>{ value }</button>);
+function Square({value, onSquareClick}) {
+  return <button className="square" onClick={onSquareClick}>{value}</button>;
 }
